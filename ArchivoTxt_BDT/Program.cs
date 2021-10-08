1bbbb6b4a923e01fe650a7eaffa2d71a2f0f01cd
Jesus Rozo <jesus.rozo@samtel.co>
2021-10-08 15:01:50 -0500

    Agregar archivos de proyecto.

---
+++ ArchivoTxt_BDT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArchivoTxt_BDT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
